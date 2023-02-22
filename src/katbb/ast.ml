b4863b56d4fcbaca7e35d067126c8cecb931a4c8
Henning Basold <henning@basold.eu>
2023-02-22 09:56:38 +0100

    Fixed data type declaration to work with newer ocam

--- src/katbb/ast.ml
+++ src/katbb/ast.ml
@@ -1,7 +1,10 @@
 open Base
 
 type test = { var: string; value: bool }
-and act = test
-and bexp = test Kat.Ast.bexp
-and exp = (act, test) Kat.Ast.exp
+  [@@deriving sexp, compare, equal, hash]
+type act = test
+  [@@deriving sexp, compare, equal, hash]
+type bexp = test Kat.Ast.bexp
+  [@@deriving sexp, compare, equal, hash]
+type exp = (act, test) Kat.Ast.exp
   [@@deriving sexp, compare, equal, hash]
